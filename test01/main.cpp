ea75df80075c566c999181f782d37bfac0e8c316
polin-sv <polinasvetlay@icloud.com>
2026-05-13 18:50:03 +0300

    test02

--- test01/main.cpp
+++ test01/main.cpp
@@ -1,40 +1,40 @@
-#include "BankAccount.h"
-
-BankAccount getRichOwner(BankAccount accounts[]) {
-	int index = 0;
-
-	for (int i = 1; i < 3; i++)
-	{
-		if (accounts[i].balance > accounts[index].balance) {
-			index = i;
-		}
-	}
-
-	return accounts[index];
-}
-
-int main() {
-	BankAccount account01;
-	BankAccount account02;
-	BankAccount account03;
-
-	account01.name = "1234AX";
-	account01.balance = 1000;
-	account01.owner = "Gleb";
-
-	account02.name = "231";
-	account02.balance = 10;
-	account02.owner = "Alex";
-
-	account03.name = "1754QA";
-	account03.balance = 500;
-	account03.owner = "Stas";
-
-	BankAccount accounts[]{ account01, account02,account03 };
-
-	BankAccount account = getRichOwner(accounts);
-
-	cout << account.owner << " - " << account.balance << endl;
-
-	return 0;
-}
+//#include "BankAccount.h"
+//
+//BankAccount getRichOwner(BankAccount accounts[]) {
+//	int index = 0;
+//
+//	for (int i = 1; i < 3; i++)
+//	{
+//		if (accounts[i].balance > accounts[index].balance) {
+//			index = i;
+//		}
+//	}
+//
+//	return accounts[index];
+//}
+//
+//int main() {
+//	BankAccount account01;
+//	BankAccount account02;
+//	BankAccount account03;
+//
+//	account01.name = "1234AX";
+//	account01.balance = 1000;
+//	account01.owner = "Gleb";
+//
+//	account02.name = "231";
+//	account02.balance = 10;
+//	account02.owner = "Alex";
+//
+//	account03.name = "1754QA";
+//	account03.balance = 500;
+//	account03.owner = "Stas";
+//
+//	BankAccount accounts[]{ account01, account02,account03 };
+//
+//	BankAccount account = getRichOwner(accounts);
+//
+//	cout << account.owner << " - " << account.balance << endl;
+//
+//	return 0;
+//}
---
+++ test01/main02.cpp
@@ -0,0 +1,42 @@
+#include "BankAccount.h"
+
+BankAccount* getRichOwner(BankAccount** accounts) {
+	int index = 0;
+
+	for (int i = 1; i < 3; i++)
+	{
+		if (accounts[i]->balance > (*(accounts + index))->balance) {
+			index = i;
+		}
+	}
+
+	return accounts[index];
+}
+
+int main() {
+	BankAccount* account01 = new BankAccount;
+	BankAccount* account02 = new BankAccount;
+	BankAccount* account03 = new BankAccount;
+
+	BankAccount* accounts[]{ account01, account02,account03 };
+
+	account01->name = "1234AX";
+	account01->balance = 1000;
+	account01->owner = "Gleb";
+
+	account02->name = "231";
+	account02->balance = 10;
+	account02->owner = "Alex";
+
+	account03->name = "1754QA";
+	account03->balance = 500;
+	account03->owner = "Stas";
+
+	
+
+	BankAccount* account = getRichOwner(accounts);
+
+	cout << account->owner << " - " << account->balance << endl;
+
+	return 0;
+}
